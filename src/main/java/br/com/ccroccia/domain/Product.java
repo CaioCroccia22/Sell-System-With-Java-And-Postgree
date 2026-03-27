09647ab4e5645421c2f100a8578460f535d8384c
caiocroccia <caiocroccia@fedora>
2026-03-27 00:29:05 -0300

    feat: add quantity to product class

--- src/main/java/br/com/ccroccia/domain/Product.java
+++ src/main/java/br/com/ccroccia/domain/Product.java
@@ -1,25 +1,41 @@
 package br.com.ccroccia.domain;
 
-import annotation.Column;
-import annotation.Table;
+import annotation.*;
+import br.com.ccroccia.dao.Persistent;
 
 @Table(tableName = "Product")
-public class Product {
+public class Product implements Persistent {
 
-	@Column(columnName = "cd_produto", method = "setId")
-	private Integer id;
-	@Column(columnName = "ds_produto", method = "setDescription")
+	@KeyType("getId")
+	@Column(columnName = "cd_product", method = "setId")
+	private Long id;
+
+	@Column(columnName = "nm_product", method = "setName")
+	private String name;
+
+	@Column(columnName = "ds_product", method = "setDescription")
 	private String description;
-	@Column(columnName = "qtd_produto", method = "setQuantity")
+
+	@Column(columnName = "vl_price", method = "setPrice")
+	private Double price;
+
+	@Column(columnName = "qtd_product", method = "setQuantity")
 	private Integer quantity;
 
-
-	public Integer getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(Integer id) {
+	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	public String getDescription() {
@@ -28,6 +44,14 @@ public class Product {
 
 	public void setDescription(String description) {
 		this.description = description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
 	}
 
 	public Integer getQuantity() {
